b96e948ca3ea469ef46468fbb89f05421b62780d
Nzzz964 <56777838+Nzzz964@users.noreply.github.com>
2025-05-20 12:49:36 +0800

    update:

--- arceos/payload/skernel2/src/main.rs
+++ arceos/payload/skernel2/src/main.rs
@@ -5,6 +5,9 @@ use core::panic::PanicInfo;
 
 #[no_mangle]
 unsafe extern "C" fn _start() -> ! {
+    // csrr is a pseudo-instructions where rs1 = x0
+    // csrrw rd, csr, rs1
+    // dataflow rd <- csr <- rs1
     core::arch::asm!(
         "csrr a1, mhartid",
         "ld a0, 64(zero)",
